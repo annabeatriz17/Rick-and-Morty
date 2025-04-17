b89ea24116912be670f05025a11e07ea84331d34
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-17 10:56:46 -0300

    update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -8,8 +8,8 @@ import styles from "../home/Home.module.css";
 
 export default function Home() {
     const [search, setSearch] = useState("");
-    const [notFound, setNotFound] = useState(false);
     const [characters, setCharacters] = useState([]);
+    const [notFound, setNotFound] = useState(false);
 
     useEffect(() => {
         const fetchCharacters = async (name = "") => {
@@ -22,15 +22,44 @@ export default function Home() {
                 setCharacters([]);
             }
         };
-        fetchCharacters();
+        useEffect(() => {
+            fetchCharacters();
     }, []);
+
+    const handleCardClick = (name) => {
+        toast.info(`Você clicou em ${name}`, {
+        });
+    }
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Personagens de Rick and Morty</h1>
+            <div className={styles.controls}>
+                <input
+                    type="text"
+                    placeholder="Buscar personagem"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)} className={styles.input}
+                />
+                <button
+                onClick={() => fetchCharacters(search.trim())}className={styles.buttonSearch}
+                >
+                Buscar
+                </button>
+                <button
+                    onClick={() => {
+                        setSearch("");
+                        fetchCharacters();
+                    }}
+                    className={styles.buttonReset}
+                >
+                    Resetar
+                </button>
+            </div>
+            {notFound && <h1 className={styles.notFound}>Nenhum personagem encontrado 😢</h1>}
             <div className={styles.grid}>
             {characters.map((char) => (
-                <CharacterCard key={char.id} characters={char} />
+                <CharacterCard key={char.id} characters={char}  onClick={() => handleCardClick(char.nome)}/>
             ))}
             </div>
         </div>
