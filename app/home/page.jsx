064f7b8290e39f0c6c59e3b0dbe50414702fa9ea
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-17 09:52:52 -0300

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,23 +1,27 @@
 "use client";
 
-import styles from "../home/Home.module.css";
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 import CharacterCard from "../../componentes/CharacterCard";
+import styles from "../home/Home.module.css";
 
 export default function Home() {
+    const [search, setSearch] = useState("");
+    const [notFound, setNotFound] = useState(false);
     const [characters, setCharacters] = useState([]);
 
     useEffect(() => {
-        axios
-        .get("https://rickandmortyapi.com/api/character/")
-        .then((response) => {
-            setCharacters(response.data.results);
-        })
-        .catch ((error) => {
-            console.error('Erro ao buscar os personagens:', error);
-        });
+        const fetchCharacters = async (name = "") => {
+            try {
+                const response = await axios.get("https://rickandmortyapi.com/api/character/?name=${name}");
+                setCharacters(response.data.results);
+            } catch (error) {
+                console.error("Erro ao buscar os personagens:", error);
+            }
+        };
+        fetchCharacters();
     }, []);
 
     console.log(characters);
