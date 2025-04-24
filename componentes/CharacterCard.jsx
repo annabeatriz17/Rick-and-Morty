175d96d813abf064abc9de4ab79d4e539d986887
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-24 10:23:19 -0300

    update

--- componentes/CharacterCard.jsx
+++ componentes/CharacterCard.jsx
@@ -1,15 +1,15 @@
 import styles from "../styles/CharacterCard.module.css";
 
-export default function CharacterCard({ characters, onClick }) {
+export default function CharacterCard({ character, onClick }) {
     return (
-    <div className={styles.card} onClick={onClick}>
-        <img src={characters.image} alt={characters.name}className={styles.avatar}
-        />
-        <h3 className={styles.title}>{characters.name}</h3>
-        <p>{characters.status}</p>
-        <p>{characters.species}</p>
-        <p>{characters.type || "Sem tipo"}</p>
-        <p>{characters.gender}</p>
-    </div>
+        <div className={styles.card} onClick={onClick}>
+            <img src={character.image} alt={character.name} className={styles.avatar} />
+            <h3 className={styles.title}>{character.name}</h3>
+            <p>{character.status}</p>
+            <p>{character.species}</p>
+            <p>{character.type || "Sem Tipo"}</p>
+            <p>{character.gender}</p>
+        </div>
     );
-}   
+}
+        
